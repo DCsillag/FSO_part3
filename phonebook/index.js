ebcb93c8402306bf9a1074809a856864751848ca
Daniel Csillag <danielcsillag@hotmail.com>
2025-05-22 12:42:04 +1000

    udpated to include advanced validation and type checking

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,13 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+const Person = require('./models/Person')
 
+// Define middleware
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
 // Custom Morgan token for POST data
+/*
 morgan.token('post-data', (req) => {
   if (req.method === 'POST') {
     return JSON.stringify(req.body);
@@ -39,13 +43,87 @@ let data = [
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+]*/
 // Send all data in the data object via JSON. 
-app.get('/api/persons', (request,response) => {
-    response.json(data)
+app.get('/api/persons', (request,response,next) => {  
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
+})
+
+// Send the data for one person
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Name or Number Missing' })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+  .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const {name, number} = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      response.set('Content-type', 'text/html')
+      response.send(
+        `Phonebook has info for ${count} people <br> 
+        ${new Date()}`
+      )
+    })
+    .catch(error => next(error))
 })
 
 // Send textual data about the number of people and time.
+/* Old methods that are built for a non DB / local backend 
 app.get('/info', (request, response) => {
     response.set('Content-type', 'text/html')
     response.send(
@@ -95,8 +173,30 @@ app.post('/api/persons', (request, response) => {
 
     response.json(person)
 })
+    */
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ phonebook/models/Person.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(error => {
+        console.log('Error connecting to DB', error.message)
+    })
+
+const isAustralianPhone = v => {
+    if (typeof v !== 'string') return false;
+    const cleaned = v.replace(/[\s\-().]/g, '');
+    return /^(\+61|0)[23784]\d{8}$/.test(cleaned);
+};
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    number: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        validate: {
+          validator: isAustralianPhone,
+          message: props => `${props.value} is not a valid Australian phone number!`
+        }
+      }
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
